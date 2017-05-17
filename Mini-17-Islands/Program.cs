cb5131fb797c495b48d5a644fd1ae406edac44d5
encid <ryancav@gmail.com>
2017-05-17 13:20:26 -0400

    Add project files.

---
+++ Mini-17-Islands/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "proogrrrammmingggarrrr".ToCharArray();
+            foreach (var item in get_islands(input))
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+
+        static Dictionary<char,int> get_islands(char[] input)
+        {
+            var islands = new Dictionary<char,int> { };
+            var count = 0;
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] == input[i - 1])
+                    count++;
+                else
+                    count = 0;
+                if (count > 0 && !islands.Keys.Contains(input[i - 1]))
+                {
+                    islands[input[i - 1]] = i - count;
+                }
+            }
+
+            return islands;
+        }
+    }
+}
